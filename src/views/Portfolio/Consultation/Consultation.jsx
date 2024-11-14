0dbf5f62eb1d4f06d66ea01865f917054022b066
Valentina Galeano <tinasevenofficial@gmail.com>
2024-11-14 07:53:41 -0500

    Update Consultas cartera

--- src/views/Portfolio/Consultation/Consultation.jsx
+++ src/views/Portfolio/Consultation/Consultation.jsx
@@ -1,126 +1,130 @@
-/*eslint-disable*/
-import React, { useState, useMemo, useEffect } from 'react'
+/* eslint-disable */
+import React, { useState, useMemo, useEffect } from 'react';
+import axios from 'axios';
 
-import axios from 'axios'
+import { useCRUD, useBoolean, useFetch } from 'hooks';
+import Card from 'components/Card/Card';
+import CardBody from 'components/Card/CardBody';
+import CardHeader from 'components/Card/CardHeader';
+import CardText from 'components/Card/CardText';
+import globalStyles from 'assets/jss/material-dashboard-pro-react/views/common';
 
-import { useCRUD, useBoolean, useFetch } from 'hooks'
-import Card from 'components/Card/Card'
-import CardBody from 'components/Card/CardBody'
-import CardHeader from 'components/Card/CardHeader'
-import CardText from 'components/Card/CardText'
-import globalStyles from 'assets/jss/material-dashboard-pro-react/views/common'
+import { Filter, Details, Table, DetailsClient, TableWithoutClient } from './components';
 
-import { Filter, Details, Table, DetailsClient, TableWithoutClient } from './components'
-
-const portfolioPerAge = [18305000, 24305000, 54305000, 14305000]
+const portfolioPerAge = [18305000, 24305000, 54305000, 14305000];
 
 const ConsultationPortfolio = () => {
-  const globalClasses = globalStyles()
-  const { data, loading } = useFetch('/api/creditos')
+  const globalClasses = globalStyles();
+  const { data, loading, error } = useFetch('/api/creditos');
 
-  const { fields, remove, append, update, replace } = useCRUD([])
-  const [selectedClient, setSelectedClient] = useState('all')
-  const [selectedShowClient, setSelectedShowClient] = useState()
-  const [fieldsCreditClient, setFieldsCreditClient] = useState()
-  const [dataOnlyClient, setDataOnlyClient] = useState()
-  const [subLoading, setSubLoading] = useState(false)
-  const [open, setOpen] = useBoolean(false)
+  const { fields, remove, append, update, replace } = useCRUD([]);
+  const [selectedClient, setSelectedClient] = useState('all');
+  const [selectedShowClient, setSelectedShowClient] = useState(null); 
+  const [fieldsCreditClient, setFieldsCreditClient] = useState([]);
+  const [dataOnlyClient, setDataOnlyClient] = useState([]);
+  const [subLoading, setSubLoading] = useState(false);
+  const [open, setOpen] = useBoolean(false);
 
   const handleSelected = ({ target }) => {
-    console.log(target.value)
-    setSelectedClient(target.value)
-  }
+    console.log(target.value);
+    setSelectedClient(target.value);
+  };
 
   useEffect(() => {
-    if (data) replace(data.clientes)
-  }, [data])
+    if (data && data.clientes) {
+      replace(data.clientes);
+    } else if (error) {
+      console.error("Error al obtener clientes:", error);
+    }
+  }, [data, error, replace]);
 
   useEffect(() => {
     if (selectedClient !== 'all') {
-      credits_client()
+      credits_client();
     }
-  }, [selectedClient])
+  }, [selectedClient]);
 
   const client = useMemo(
     () =>
       selectedClient !== 'all'
         ? fields.find(({ id_clientes }) => id_clientes === selectedClient)
-        : {
-          id_clientes: '0',
-          nombres: 'TODOS',
-          apellidos: '',
-        },
+        : { id_clientes: '0', nombres: 'TODOS', apellidos: '' },
     [selectedClient, fields]
-  )
+  );
 
-  const showClient = useMemo(() => fields.find(({ id_clientes }) => id_clientes === selectedShowClient), [
-    selectedShowClient,
-    fields,
-  ])
+  const showClient = useMemo(
+    () => fields.find(({ id_clientes }) => id_clientes === selectedShowClient),
+    [selectedShowClient, fields]
+  );
 
   const credits_client = () => {
-    setSubLoading(true)
-    axios.get(`api/creditos_cliente/${selectedClient}`)
-      .then(res => {
-        setFieldsCreditClient(res.data)
-        setDataOnlyClient(res.data)
+    setSubLoading(true);
+    axios
+      .get(`/api/creditos_cliente/${selectedClient}`)
+      .then((res) => {
+        setFieldsCreditClient(res.data || []);
+        setDataOnlyClient(res.data || []);
         setSubLoading(false);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.error("Error al obtener créditos del cliente:", err);
+        setSubLoading(false);
+      });
+  };
 
-  const onOpen = id => {
-    setSelectedShowClient(id)
-    setOpen(true)
-  }
+  const onOpen = (id) => {
+    setSelectedShowClient(id);
+    setOpen(true);
+  };
 
   const onClose = () => {
-    setSelectedShowClient(before => (before === undefined ? null : undefined))
-    setOpen(false)
-  }
+    setSelectedShowClient(null);  
+    setOpen(false);
+  };
 
-  const create = data => {
+  const create = (data) => {
     axios
-      .post('api/clientes', data)
-      .then(res => {
-        //append(res.data)//Preguntar si aqui agrega para que se vea en la tabla
-        onClose()
+      .post('/api/clientes', data)
+      .then((res) => {
+        append(res.data); //Preguntar si aqui agrega para que se vea en la tabla
+        onClose();
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.error("Error al crear cliente:", err);
+      });
+  };
 
   const edit = (id, data) => {
     axios
-      .put(`api/clientes/${id}`, data)
-      .then(res => {
-        update(id, res.data)
-        onClose()
+      .put(`/api/clientes/${id}`, data)
+      .then((res) => {
+        update(id, res.data);
+        onClose();
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.error("Error al editar cliente:", err);
+      });
+  };
 
-  const _delete = id => {
+  const _delete = (id) => {
     axios
-      .delete(`api/clientes/${id}`)
+      .delete(`/api/clientes/${id}`)
       .then(() => {
-        remove(id)
-        onClose()
+        remove(id);
+        onClose();
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.error("Error al eliminar cliente:", err);
+      });
+  };
 
   const onSave = (data, id) => {
-    if (id) edit(id, data)
-    else create(data)
-  }
+    if (id) {
+      edit(id, data);
+    } else {
+      create(data);
+    }
+  };
 
   return (
     <Card>
@@ -131,16 +135,25 @@ const ConsultationPortfolio = () => {
       </CardHeader>
       <CardBody>
         <Filter clients={fields} loading={loading} selected={selectedClient} handleSelected={handleSelected} />
-        {!loading && !subLoading && <Details client={client} data={data} selected={selectedClient} dataOnlyClient={dataOnlyClient} loading={selectedClient !== 'all' ? subLoading : loading} portfolioPerAge={portfolioPerAge} />}
+        {!loading && !subLoading && (
+          <Details
+            client={client}
+            data={data}
+            selected={selectedClient}
+            dataOnlyClient={dataOnlyClient}
+            loading={selectedClient !== 'all' ? subLoading : loading}
+            portfolioPerAge={portfolioPerAge}
+          />
+        )}
         {selectedClient === 'all' ? (
           <TableWithoutClient clients={fields} loading={loading} onDelete={_delete} onOpen={onOpen} />
         ) : (
-          <Table client={fieldsCreditClient} loading={subLoading}/>
+          <Table client={fieldsCreditClient} loading={subLoading} />
         )}
         <DetailsClient open={open} onClose={onClose} onSubmit={onSave} client={showClient} />
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default ConsultationPortfolio
+export default ConsultationPortfolio;
