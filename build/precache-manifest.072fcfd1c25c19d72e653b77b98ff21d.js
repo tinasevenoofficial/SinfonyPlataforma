51f2874dc5d5c03024fe1f5f5f23107e0df89695
Valentina Galeano <tinasevenofficial@gmail.com>
2024-12-04 12:24:40 -0500

    Refactor

---
+++ build/precache-manifest.072fcfd1c25c19d72e653b77b98ff21d.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def2a259c94b70577dfd095d22820677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b28df706806ebd9a6291",
+    "url": "/static/css/2.834c58a9.chunk.css"
+  },
+  {
+    "revision": "3a073e19b38c898d157c",
+    "url": "/static/css/main.f06f92be.chunk.css"
+  },
+  {
+    "revision": "b28df706806ebd9a6291",
+    "url": "/static/js/2.6b3fe4db.chunk.js"
+  },
+  {
+    "revision": "8950a610cd70b31aa4e48daed6e9f6ad",
+    "url": "/static/js/2.6b3fe4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31aeb66ff70d67969404",
+    "url": "/static/js/3.51d5a216.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.51d5a216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf7a4203fa2a56530c0",
+    "url": "/static/js/4.b46a2960.chunk.js"
+  },
+  {
+    "revision": "17edd1ad6c21ec64c58fd7bdf80e1f65",
+    "url": "/static/js/4.b46a2960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a073e19b38c898d157c",
+    "url": "/static/js/main.4441b84e.chunk.js"
+  },
+  {
+    "revision": "d3d20b1213741f29ddd886dbbf9d8c3a",
+    "url": "/static/js/main.4441b84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc243e8667bfbe709c2",
+    "url": "/static/js/runtime-main.481d0dcd.js"
+  },
+  {
+    "revision": "ccc19a0706d8c43cf52d12acc98fa9fe",
+    "url": "/static/media/LogoDualNota10bga.ccc19a07.png"
+  },
+  {
+    "revision": "8835284724bdb8e8c035ea208036e277",
+    "url": "/static/media/LogoDualNota12cal.88352847.png"
+  },
+  {
+    "revision": "aa3844d6a0a510ded030ac5d45527924",
+    "url": "/static/media/LogoDualNota14med.aa3844d6.png"
+  },
+  {
+    "revision": "f01b7bb2b23d467c4678efc34b758f10",
+    "url": "/static/media/LogoDualNota16bta.f01b7bb2.png"
+  },
+  {
+    "revision": "2ee59272cd1aa718e4dd702a9685d14f",
+    "url": "/static/media/LogoDualNota1mon.2ee59272.png"
+  },
+  {
+    "revision": "a29a545980ac4ef2d0a615db579a2b18",
+    "url": "/static/media/LogoDualNota21cal.a29a5459.png"
+  },
+  {
+    "revision": "d6611599c938be10f839dd5c8af3ca39",
+    "url": "/static/media/LogoDualNota26bta.d6611599.png"
+  },
+  {
+    "revision": "3e84edb2c4d8bf78dc275bb61703c6f6",
+    "url": "/static/media/LogoDualNota27bta.3e84edb2.png"
+  },
+  {
+    "revision": "4aa8c4aa608998ef71301cdeae2ddf6b",
+    "url": "/static/media/LogoDualNota2bga.4aa8c4aa.png"
+  },
+  {
+    "revision": "163f111682b8e2df64032e2d059f5aec",
+    "url": "/static/media/LogoDualNota2flo.163f1116.png"
+  },
+  {
+    "revision": "7e9188fce3a7cae22cc4d207ab1cd8ac",
+    "url": "/static/media/LogoDualNota2rio.7e9188fc.png"
+  },
+  {
+    "revision": "3f24f772936174306989e943ba6da3b5",
+    "url": "/static/media/LogoDualNota2zip.3f24f772.png"
+  },
+  {
+    "revision": "92166d71f0021484b9939c8d155358a5",
+    "url": "/static/media/LogoDualNota35bta.92166d71.png"
+  },
+  {
+    "revision": "041bbf35a778f6d0cd85cd9380df6234",
+    "url": "/static/media/LogoDualNota3bga.041bbf35.png"
+  },
+  {
+    "revision": "b74a24d95ea60cc58b54c83461ef09f3",
+    "url": "/static/media/LogoDualNota3env.b74a24d9.png"
+  },
+  {
+    "revision": "22befa173c98c9d89475e7172b9d2cb8",
+    "url": "/static/media/LogoDualNota4cal.22befa17.png"
+  },
+  {
+    "revision": "0641cda24b72a88d906e7559c528fe63",
+    "url": "/static/media/LogoDualNota51bta.0641cda2.png"
+  },
+  {
+    "revision": "3612ca7f24c5310f74aaf161aa5c48c9",
+    "url": "/static/media/LogoDualNota5bga.3612ca7f.png"
+  },
+  {
+    "revision": "a6e47fbe7a8121572e870502aaf7e7dd",
+    "url": "/static/media/LogoDualNota5cal.a6e47fbe.png"
+  },
+  {
+    "revision": "11be957a7e7de111a3193285ef79461c",
+    "url": "/static/media/LogoDualNota72bta.11be957a.png"
+  },
+  {
+    "revision": "691f3cd33641f3de5bf971335c594393",
+    "url": "/static/media/LogoDualNota9bga.691f3cd3.png"
+  },
+  {
+    "revision": "34288fb2dea8362b9981961eebf04798",
+    "url": "/static/media/LogoDualNota9cal.34288fb2.png"
+  },
+  {
+    "revision": "a603ca421ecf54bf8ae0495700a7782c",
+    "url": "/static/media/LogoDualNotadev.a603ca42.png"
+  },
+  {
+    "revision": "3a343e32defd339c0b4e4addd2688d89",
+    "url": "/static/media/LogoPrincipalNota10bga.3a343e32.png"
+  },
+  {
+    "revision": "74710984706f00891cb735ef4020483a",
+    "url": "/static/media/LogoPrincipalNota12cal.74710984.png"
+  },
+  {
+    "revision": "8d500ba97ace93ea51a15849772bbf79",
+    "url": "/static/media/LogoPrincipalNota14med.8d500ba9.png"
+  },
+  {
+    "revision": "749f9e4284c8d4bbf61354fd1e34acdc",
+    "url": "/static/media/LogoPrincipalNota16bta.749f9e42.png"
+  },
+  {
+    "revision": "70ed91ee569e0374c1fe89d0e1ed8161",
+    "url": "/static/media/LogoPrincipalNota1mon.70ed91ee.png"
+  },
+  {
+    "revision": "b75cf5de98be8e729c2732be2419a6f9",
+    "url": "/static/media/LogoPrincipalNota21cal.b75cf5de.png"
+  },
+  {
+    "revision": "9b12b0ce01672e3731f31fd1b662c1a5",
+    "url": "/static/media/LogoPrincipalNota26bta.9b12b0ce.png"
+  },
+  {
+    "revision": "61cc1af39759da8571541737372030ce",
+    "url": "/static/media/LogoPrincipalNota27bta.61cc1af3.png"
+  },
+  {
+    "revision": "3cfa7fc5ae8e563e08506d589845faee",
+    "url": "/static/media/LogoPrincipalNota2bga.3cfa7fc5.png"
+  },
+  {
+    "revision": "fd216dfdd6c2709a5293e67a02c9e24c",
+    "url": "/static/media/LogoPrincipalNota2flo.fd216dfd.png"
+  },
+  {
+    "revision": "873585c78296f0c468f7dc35379eb900",
+    "url": "/static/media/LogoPrincipalNota2rio.873585c7.png"
+  },
+  {
+    "revision": "380780794400abde1617d4af846c5402",
+    "url": "/static/media/LogoPrincipalNota2zip.38078079.png"
+  },
+  {
+    "revision": "f7edeecb53c2933791d4f1b6d612f322",
+    "url": "/static/media/LogoPrincipalNota35bta.f7edeecb.png"
+  },
+  {
+    "revision": "14545be380d0ad4c8f6697d7129237b4",
+    "url": "/static/media/LogoPrincipalNota3bga.14545be3.png"
+  },
+  {
+    "revision": "0586f242dc00f4329dbf106ab1fff951",
+    "url": "/static/media/LogoPrincipalNota3env.0586f242.png"
+  },
+  {
+    "revision": "54900c3b20b9c04e141491f1a24822e7",
+    "url": "/static/media/LogoPrincipalNota4cal.54900c3b.png"
+  },
+  {
+    "revision": "e5a48d639b8b4a0f3e7d7a3b5fb7434d",
+    "url": "/static/media/LogoPrincipalNota51bta.e5a48d63.png"
+  },
+  {
+    "revision": "fc107ca608e78d043326daa8ae545d3e",
+    "url": "/static/media/LogoPrincipalNota5bga.fc107ca6.png"
+  },
+  {
+    "revision": "cdf5a97cfdb3e780aec2453c9b825f53",
+    "url": "/static/media/LogoPrincipalNota5cal.cdf5a97c.png"
+  },
+  {
+    "revision": "a86c2d5a0c7e8b82df005b035e092e2a",
+    "url": "/static/media/LogoPrincipalNota72bta.a86c2d5a.png"
+  },
+  {
+    "revision": "5b39c38451cc4a43c88f985de5bd2a22",
+    "url": "/static/media/LogoPrincipalNota9bga.5b39c384.png"
+  },
+  {
+    "revision": "7be0ca342c667487a7934d45a962922f",
+    "url": "/static/media/LogoPrincipalNota9cal.7be0ca34.png"
+  },
+  {
+    "revision": "e59fa39dc8d1b426e7452c260e625d6e",
+    "url": "/static/media/LogoPrincipalNotadev.e59fa39d.png"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "c72dfa7018d295305acc5ec4666fc940",
+    "url": "/static/media/doc.c72dfa70.svg"
+  },
+  {
+    "revision": "21b5f807c988c3adcc8d7e07a26f79bd",
+    "url": "/static/media/headPDF.21b5f807.png"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "0a1be3de4a637aa22113e296881e54f2",
+    "url": "/static/media/login.0a1be3de.jpeg"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "ab6c92ec8d0cc093b6d820a5f894ba10",
+    "url": "/static/media/prueba.ab6c92ec.pdf"
+  },
+  {
+    "revision": "62553e2e746f3e7cec58475188c614b2",
+    "url": "/static/media/qrPDF.62553e2e.png"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "7cb221bfaf33f2a2d3000a7632983df3",
+    "url": "/static/media/sinfonyLogo.7cb221bf.png"
+  },
+  {
+    "revision": "3f582932c6eaf42138814d53eef5b921",
+    "url": "/static/media/sinfonyLogoG.3f582932.png"
+  },
+  {
+    "revision": "7dd91fa266b215a473158b502f8d9fd3",
+    "url": "/static/media/supernotariado.7dd91fa2.png"
+  },
+  {
+    "revision": "5f29b4291f1cad5d29b2b1f7da621f1d",
+    "url": "/static/media/ucnc.5f29b429.png"
+  }
+]);
